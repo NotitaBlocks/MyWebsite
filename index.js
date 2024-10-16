2d8526500d26a813addb6b1dc974ce4034badb9f
NotitaBlocks <maicolfarfan1@gmail.com>
2024-10-16 12:07:56 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburguesa = document.getElementById("hamburguesa");
+    const menu = document.getElementById("menu");
+
+    hamburguesa.addEventListener("click", () => {
+        menu.classList.toggle("mostrar");
+    });
+});
